f584b71114aa7c923fdcf67a2ee63ae379e5bcbb
indianakv@gmail.com <Schumacher4u>
2019-08-20 17:29:43 -0400

    Pascal's Triangle

---
+++ src/main/java/com/kv/algorithm/day01/PascalTriangle.java
@@ -0,0 +1,45 @@
+package com.kv.algorithm.day01;
+
+public class PascalTriangle {
+
+    static int[] compute(int row){
+
+        if(row == 0){
+            int[] zero = {1};
+            return zero;
+        }
+
+        int[] one = {1,1};
+        if(row == 1){
+            return one;
+        }
+
+        int[] previous = one;
+
+        for(int r = 2; r <= row; r++ ){
+
+            int[] next = new int[r+1];
+            next[0] = 1;
+
+            for(int i = 1; i< r; i++){
+                next[i] = previous[i -1]+previous[i];
+            }
+            next[r] = 1;
+            previous = next;
+        }
+        return  previous;
+    }
+
+    public static void main(String[] args) {
+        int rows = 5;
+        int count = 5;
+        for(int i =0; i<rows; i++){
+            System.out.format("%1$"+count+"s","");
+            count--;
+            for(int j : compute(i)){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
