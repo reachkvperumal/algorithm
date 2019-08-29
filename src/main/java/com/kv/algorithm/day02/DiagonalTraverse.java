c34afa30da6405c72c27036114d5688b229b2702
indianakv@gmail.com <Schumacher4u>
2019-08-29 00:04:53 -0400

    matrix traverse

--- src/main/java/com/kv/algorithm/day02/DiagonalTraverse.java
+++ src/main/java/com/kv/algorithm/day02/DiagonalTraverse.java
@@ -1,8 +1,5 @@
 package com.kv.algorithm.day02;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class DiagonalTraverse {
 
     /**
@@ -13,33 +10,39 @@ public class DiagonalTraverse {
      * ]
      */
 
-    static void compute(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
+    static int[] compute(int[][] matrix) {
+        if (matrix.length == 0)
+            return new int[0];
 
-        int[] nArray = new int[r * c];
+        int maxRow = matrix.length;
+        int maxCol = matrix[0].length;
+        int[] result = new int[maxRow * maxCol];
+        int row = 0, col = 0;
 
-        int currentRow = 0;
-        int currentColumn = 0;
-        List<Integer> diagonal = new ArrayList<>();
-        for (int i = 0; i < r; i++) {
-            diagonal.add(matrix[currentRow][currentColumn]);
-            if (currentColumn < c)
-                currentColumn++;
-
-            diagonal.add(matrix[currentRow][currentColumn]);
-
-            if (currentRow < r)
-                currentRow++;
-
-            diagonal.add(matrix[currentRow][currentColumn]);
-
-
+        for (int i = 0; i < result.length; i++) {
+            result[i] = matrix[row][col];
+            if ((row + col) % 2 == 0) {
+                if (col == maxCol - 1) {
+                    row++;
+                } else if (row == 0) {
+                    col++;
+                } else {
+                    col++;
+                    row--;
+                }
+            } else {
+                if (row == maxRow - 1) {
+                    col++;
+                } else if (col == 0) {
+                    row++;
+                } else {
+                    col--;
+                    row++;
+                }
+            }
         }
-
-
+        return result;
     }
-
 
     public static void main(String[] args) {
         int[][] matrix = {
@@ -48,8 +51,20 @@ public class DiagonalTraverse {
                 {11, 12, 13, 14, 15},
                 {16, 17, 18, 19, 20}
         };
-
+        int[][] matrix1 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
 //Output:  [1,2,4,7,5,3,6,8,9]
+// Output:  [1,2,6,11,7,3,4,8,12,16,17,13,9,5,10,14,18,19,15,20]
 
+        for (int i : compute(matrix1))
+            System.out.print(i + ",");
+
+        System.out.println();
+
+        for (int i : compute(matrix))
+            System.out.print(i + ",");
     }
 }
