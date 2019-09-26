5448ccf5eaa4e8d4a454de547b8d6c9b18335733
indianakv@gmail.com <Schumacher4u>
2019-09-25 23:12:35 -0400

    Finished - check sum of sub set of a set equals target

---
+++ src/main/java/com/kv/algorithm/day14/RecursionTest.java
@@ -0,0 +1,31 @@
+package com.kv.algorithm.day14;
+
+public class RecursionTest {
+
+    static boolean compute(long[] arr, long k, int start, boolean left) {
+
+        if (left && k == 0)
+            return true;
+
+        if (arr.length == start)
+            return false;
+        else {
+            if (arr[start] == k)
+                return true;
+            else
+                return compute(arr, k, start + 1, left) || compute(arr, k - arr[start], start + 1, true);
+        }
+    }
+
+
+    static boolean check_if_sum_possible(long[] arr, long k) {
+        return compute(arr, k, 0, false);
+    }
+
+
+    public static void main(String[] args) {
+        long[] input = {1l, 2l, 3l, 9l, 5l, 9l, 13l};
+
+        System.out.println(check_if_sum_possible(input, 12l));
+    }
+}
